6fd4680f270d4c3bbd3cb9d6f2e487bf17cc2dcc
Chloé Maurel <maurel.chloe@gmail.com>
2019-05-10 14:41:44 +0200

    First Commit new repository

---
+++ app/src/main/java/com/example/julienelkaim/enap/MainDispatcherActivity.java
@@ -0,0 +1,13 @@
+package com.example.julienelkaim.enap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainDispatcherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dispatcher);
+    }
+}
